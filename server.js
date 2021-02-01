a6f0b78dc4fa41642bd003ed8926f68ff06cd49d
Captain <71038018+karimi65@users.noreply.github.com>
2021-02-01 13:45:29 -0600

    Adds /api/characters:routeName

--- server.js
+++ server.js
@@ -41,6 +41,15 @@ app.get('/api/characters', (req, res) => {
     res.json(characters)
 })
 
+// /api/characters/:routeName
+app.get('/api/characters/:routeName', (req, res) => {
+    const targetCharacter = req.params.routeName
+    const character = characters.find(character => {
+        return character.routeName === targetCharacter
+    })
+    res.json(character)
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
 })
